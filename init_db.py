dc42d1b3b5b5ca5ee8503e49354b209a51f11b4f
YongHyunKing <yhkim8246@gmail.com>
2023-04-04 17:05:35 +0900

    first

---
+++ init_db.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.krafton
+
+
+
+# 기존 mystar 콜렉션을 삭제하고, 출처 url들을 가져온 후, 크롤링하여 DB에 저장합니다.
+def insert_all():
+    db.test.drop()  # mystar 콜렉션을 모두 지워줍니다.
+    doc = {
+        "date": "2022.05.01",
+        "inOut": "in",
+        "type": "profit",
+        "item": "용돈",
+        "price": 300000
+    }
+    db.test.insert_one(doc)
+
+
+### 실행하기
+insert_all()
